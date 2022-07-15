0a8dbdf23c8fcae06c7a7d8ec0feaf3c1dde9b76
miyauchi-yu <ga14031521m@gmail.com>
2022-07-15 22:04:10 +0900

    access ranking bug fix

--- pages/api/addAccessInfo.ts
+++ pages/api/addAccessInfo.ts
@@ -9,6 +9,12 @@ const AddAccessInfo = (req: NextApiRequest, res: NextApiResponse<Res>) => {
     const url = req.query.url
     const data = url + '\n'
 
+    // アクセス情報ファイルが存在しない場合
+    if (!fs.existsSync('./public/data/access')) {
+        // アクセス情報ファイルを作成
+        fs.writeFileSync('./public/data/access', '')
+    }
+
     // アクセス情報ファイルへ書き込み
     fs.appendFileSync('./public/data/access', data)
 
